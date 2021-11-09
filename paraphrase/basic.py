3f64326553ae25173e829bda2caf4d10787fabd1
Siddhant Ray <siddhant.r98@gmail.com>
2021-11-09 18:47:37 +0100

    compute sentence embeddings

--- paraphrase/basic.py
+++ paraphrase/basic.py
@@ -1,5 +1,6 @@
 import json 
 import random
+from sentence_transformers import SentenceTransformer
 
 file_path_1 = "paraphrase/MSRParaphraseCorpus/msr_paraphrase_train.txt"
 file_path_2 = "paraphrase/multinli_1.0/multinli_1.0_train.jsonl"
@@ -58,10 +59,21 @@ print(final_list_of_pairs[0:5])
 #print(len(nli_pairs_list))
 #print(nli_pairs_list[0:5])
 
+model = SentenceTransformer('all-MiniLM-L6-v2', device = 'cuda')    
+train_labels = [int(item[0]) for item in final_list_of_pairs] 
+sentence_1_list = [item[1][0] for item in final_list_of_pairs]
+sentence_2_list = [item[1][1] for item in final_list_of_pairs]
+
+#print(len(train_labels),len(sentence_1_list),len(sentence_2_list))
+embeddings_1 = model.encode(sentence_1_list)
+embeddings_2 = model.encode(sentence_2_list)
+
+with open('embeddings_1.pkl', "wb") as fOut1:
+    pickle.dump({'sentences': sentences_1_list, 'embeddings': embeddings_1}, fOut1, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('embeddings_2.pkl', "wb") as fOut2:
+    pickle.dump({'sentences': sentences_2_list, 'embeddings': embeddings_2}, fOut2, protocol=pickle.HIGHEST_PROTOCOL)
     
 
-    
-    
 
-
-    
+ 
