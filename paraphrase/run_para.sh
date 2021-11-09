2adf54a95d65e219d54b8333dda49a7b7bdec1a2
Siddhant Ray <siddhant.r98@gmail.com>
2021-11-09 17:46:13 +0100

    testing paraphrase on Euler

---
+++ paraphrase/run_para.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "script to run paraphrase tasks"
+
+if ls lsf.* 1> /dev/null 2>&1
+then
+    echo "lsf files do exist"
+    echo "removing older lsf files"
+    rm lsf.*
+fi
+
+module load gcc/8.2.0 python_gpu/3.8.5 eth_proxy
+source venv_para/bin/activate
+
+if [[ $1 == "gpu" ]]
+then
+    echo "GPU mode selected"
+    bsub -n 20 -W 4:00 -R "rusage[mem=4500, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceGTX1080Ti]" python paraphrase/basic.py
+else
+    echo "CPU mode selected"
+    bsub python paraphrase/basic.py
+fi
