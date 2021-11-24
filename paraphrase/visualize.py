ba2f61a8c1c91a48e1855bd372b6f079252df10a
Siddhant Ray <siddhant.r98@gmail.com>
2021-11-24 16:53:59 +0100

    generated a visualize for tSNE

--- paraphrase/visualize.py
+++ paraphrase/visualize.py
@@ -3,6 +3,8 @@ from sklearn.manifold import TSNE
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
+
 # Load full dataset with combined NLI pairs
 with open('paraphrase/data/embeddings_1.pkl', "rb") as em1:
     stored_data_1 = pickle.load(em1)
@@ -27,17 +29,32 @@ sent_vecs_mprc_1, sent_vecs1 = stored_data_mprc_1['embeddings'], stored_data_1['
 sent_vecs_mprc_2, sent_vecs2 = stored_data_mprc_2['embeddings'], stored_data_2['embeddings']
 target_labels_mprc, target_labels = stored_labels_mprc['labels'], stored_labels['labels'],
 
-TSNE_embedded1 = TSNE(n_components = 2, perplexity = 40, n_jobs = -1, random_state = 0).fit_transform(sent_vecs_mprc_1)
+'''TSNE_embedded1 = TSNE(n_components = 2, perplexity = 40, n_jobs = -1, random_state = 0).fit_transform(sent_vecs1)
 print(TSNE_embedded1.shape)
 
-df = pd.DataFrame({'X': TSNE_embedded1[:, 0], 'Y': TSNE_embedded1[:, 1], 'Label': target_labels_mprc})
-df.to_csv('paraphrase/plots/tSNE_values_mprc_vecs1.csv', index=False)
+df = pd.DataFrame({'X': TSNE_embedded1[:, 0], 'Y': TSNE_embedded1[:, 1], 'Label': target_labels})
+df.to_csv('paraphrase/plots/tSNE_values_vecs1.csv', index=False)
 
-TSNE_embedded2 = TSNE(n_components = 2, perplexity = 40, n_jobs = -1, random_state = 0).fit_transform(sent_vecs_mprc_2)
+TSNE_embedded2 = TSNE(n_components = 2, perplexity = 40, n_jobs = -1, random_state = 0).fit_transform(sent_vecs2)
 print(TSNE_embedded2.shape)
 
-df = pd.DataFrame({'X': TSNE_embedded2[:, 0], 'Y': TSNE_embedded2[:, 1], 'Label': target_labels_mprc})
-df.to_csv('paraphrase/plots/tSNE_values_mprc_vecs2.csv', index=False)
+df = pd.DataFrame({'X': TSNE_embedded2[:, 0], 'Y': TSNE_embedded2[:, 1], 'Label': target_labels})
+df.to_csv('paraphrase/plots/tSNE_values_vecs2.csv', index=False)'''
+
+### Plot functions 
+
+files = ["paraphrase/plots/tSNE_values_mprc_vecs1.csv", "paraphrase/plots/tSNE_values_mprc_vecs2.csv", 
+        "paraphrase/plots/tSNE_values_vecs1.csv", "paraphrase/plots/tSNE_values_vecs2.csv",]
+
+for _file in files:
+    file_name_to_save = _file.split("/")[-1].replace(".csv","") + ".png"
+    df = pd.read_csv(_file)
+    fig = plt.figure(figsize=(15,15))
+    out = plt.scatter(df['X'],df['Y'], 10, c = df['Label'])
+    plt.title("tSNE on pre-classified output vs ground truth labels", size = 30)
+    plt.xticks(size = 25)
+    plt.yticks(size = 25)
+    plt.savefig("paraphrase/plots" + "/" + file_name_to_save)
 
 
 
