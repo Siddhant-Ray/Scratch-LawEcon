be721a2ea1c454519a8eeb0d5c5b354b64c81de9
Siddhant Ray <siddhant.r98@gmail.com>
2021-11-24 16:19:07 +0100

    tSNE for the inputs

---
+++ paraphrase/visualize.py
@@ -0,0 +1,45 @@
+import sys, os, json, pickle
+from sklearn.manifold import TSNE
+import numpy as np
+import pandas as pd
+
+# Load full dataset with combined NLI pairs
+with open('paraphrase/data/embeddings_1.pkl', "rb") as em1:
+    stored_data_1 = pickle.load(em1)
+with open('paraphrase/data/embeddings_2.pkl', "rb") as em2:
+    stored_data_2 = pickle.load(em2)
+with open('paraphrase/data/labels.pkl', "rb") as lbl:
+    stored_labels = pickle.load(lbl)
+
+# Load only mprc dataset 
+with open('paraphrase/data/mprc_embeddings_1.pkl', "rb") as _em1:
+    stored_data_mprc_1 = pickle.load(_em1)
+with open('paraphrase/data/mprc_embeddings_2.pkl', "rb") as _em2:
+    stored_data_mprc_2 = pickle.load(_em2)
+with open('paraphrase/data/mprc_labels.pkl', "rb") as _lbl:
+    stored_labels_mprc = pickle.load(_lbl)
+
+print(len(stored_data_mprc_1['embeddings']), type(stored_data_mprc_1['embeddings']))
+print(len(stored_data_mprc_2['embeddings']), type(stored_data_mprc_2['embeddings']))
+print(len(stored_labels_mprc['labels']), type(stored_labels_mprc['labels']))
+
+sent_vecs_mprc_1, sent_vecs1 = stored_data_mprc_1['embeddings'], stored_data_1['embeddings']
+sent_vecs_mprc_2, sent_vecs2 = stored_data_mprc_2['embeddings'], stored_data_2['embeddings']
+target_labels_mprc, target_labels = stored_labels_mprc['labels'], stored_labels['labels'],
+
+TSNE_embedded1 = TSNE(n_components = 2, perplexity = 40, n_jobs = -1, random_state = 0).fit_transform(sent_vecs_mprc_1)
+print(TSNE_embedded1.shape)
+
+df = pd.DataFrame({'X': TSNE_embedded1[:, 0], 'Y': TSNE_embedded1[:, 1], 'Label': target_labels_mprc})
+df.to_csv('paraphrase/plots/tSNE_values_mprc_vecs1.csv', index=False)
+
+TSNE_embedded2 = TSNE(n_components = 2, perplexity = 40, n_jobs = -1, random_state = 0).fit_transform(sent_vecs_mprc_2)
+print(TSNE_embedded2.shape)
+
+df = pd.DataFrame({'X': TSNE_embedded2[:, 0], 'Y': TSNE_embedded2[:, 1], 'Label': target_labels_mprc})
+df.to_csv('paraphrase/plots/tSNE_values_mprc_vecs2.csv', index=False)
+
+
+
+
+
