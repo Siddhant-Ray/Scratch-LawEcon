deed241e90b5bdacdfc0a54522c3ebe95c9d837f
Siddhant Ray <siddhant.r98@gmail.com>
2021-11-24 19:48:29 +0100

    utility scripts for rsync

---
+++ paraphrase/run_fetchfromremote.sh
@@ -0,0 +1,7 @@
+rsync -r sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/linearclassifiermetrics paraphrase/
+rsync -r sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/snnclassifiermetrics paraphrase/
+rsync -r sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/outputfiles paraphrase/
+
+source venv/bin/activate
+tensorboard --logdir==paraphrase/snnclassifiermetrics &
+tensorboard --logdir==paraphraselinearclassifiermetrics &
