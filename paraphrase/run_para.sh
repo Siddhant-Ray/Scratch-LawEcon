6f1b175fb133d13a6ab2217321d7e744f5258a50
Siddhant Ray <siddhant.r98@gmail.com>
2021-11-30 13:52:43 +0100

    changes to run scripts

--- paraphrase/run_para.sh
+++ paraphrase/run_para.sh
@@ -17,7 +17,7 @@ source venv_para/bin/activate
 
 args=(
     -G ls_lawecon
-    -n 1 
+    -n 4 
     -W 4:00
     -R "rusage[mem=4500]"
 )
@@ -41,8 +41,11 @@ while [ ! -z "$1" ]; do
     shift
 done
 
-bsub "${args[@]}" -oo paraphrase/outputfiles/snn.out python paraphrase/main.py selu
-bsub "${args[@]}" -oo paraphrase/outputfiles/linear.out python paraphrase/main.py lin
+# bsub "${args[@]}" -oo paraphrase/outputfiles/snn.out python paraphrase/main.py selu
+# bsub "${args[@]}" -oo paraphrase/outputfiles/linear.out python paraphrase/main.py lin
 # bsub "${args[@]}" -oo paraphrase/outputfiles/linear.out python paraphrase/main_linear.py
 # bsub "${args[@]}" python paraphrase/data_preprocessing.py
 # bsub "${args[@]}" python paraphrase/visualize.py
+
+
+bsub "${args[@]}" -oo paraphrase/outputfiles/logistic.out python paraphrase/logistic.py
--- paraphrase/run_pushtoremote.sh
+++ paraphrase/run_pushtoremote.sh
@@ -1,6 +1,5 @@
 rsync paraphrase/main.py sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/
-rsync paraphrase/main_linear.py sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/
-rsync -r paraphrase/configs sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/
+# rsync -r paraphrase/configs sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/
 
 # rsync paraphrase/visualize.py sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/
 rsync paraphrase/model.py sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/
@@ -9,3 +8,4 @@ rsync paraphrase/utils.py sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhan
 # rsync paraphrase/data_preprocessing.py sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/
 
 rsync paraphrase/run_para.sh sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/
+rsync paraphrase/logistic.py sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/
