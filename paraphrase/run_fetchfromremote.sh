4ec20136fcc8a0b07f107475cabf46f4e7110de5
Siddhant Ray <siddhant.r98@gmail.com>
2022-03-16 17:23:11 +0100

    manage requirements

--- paraphrase/run_fetchfromremote.sh
+++ paraphrase/run_fetchfromremote.sh
@@ -8,7 +8,7 @@ rm -rv paraphrase/figs
 #rsync -r sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/snnclassifiermetrics paraphrase/
 rsync -rv sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/outputfiles paraphrase/
 rsync -rv sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/paraphrase/saved_models paraphrase/
-rsync -rv sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/requirements.txt paraphrase/requirements.txt
+# rsync -rv sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/requirements.txt paraphrase/requirements.txt
 
 #source venv/bin/activate
 #tensorboard --logdir=paraphrase/snnclassifiermetrics &
