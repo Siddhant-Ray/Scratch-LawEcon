6e46450f4b5ac2fcb4ed3d6c6b778d9354191ba7
Siddhant Ray <siddhant.r98@gmail.com>
2022-06-20 14:37:36 +0200

    simplification clustering setup init

---
+++ simplification_clustering/run_fetchfromremote.sh
@@ -0,0 +1,7 @@
+rsync -rv sidray@euler.ethz.ch:/cluster/home/sidray/work/Siddhant_Ray/Scratch-LawEcon/simplification_clustering/* .
+
+
+
+
+
+
---
+++ simplification_clustering/run_para_cluster.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+echo "script to run clustertasks"
+
+if ls lsf.* 1> /dev/null 2>&1
+then
+    echo "lsf files do exist"
+    echo "removing older lsf files"
+    rm lsf.*
+fi
+
+if [[ -d runs/ ]]; then echo "removing runs"; rm -r runs/; fi
+if [[ -d snnclassifiermetrics/ ]]; then echo "removing snnclassfiermetrics"; rm -r snnclassifiermetrics/; fi
+if [[ -d linearclassifiermetrics/ ]]; then echo "removing linearclassifiermetrics"; rm -r linearclassifiermetrics/; fi
+
+module load gcc/8.2.0 python_gpu/3.8.5 eth_proxy
+source venv_para/bin/activate
+
+args=(
+    -G ls_lawecon
+    -n 4 
+    -W 4:00
+    -R "rusage[mem=6400]"
+)
+
+echo "getting into simplification directory"
+echo "removing .pyc files"
+# find . -name \*.pyc -delete
+
+if [ -z "$1" ]; then echo "CPU mode selected"; fi
+while [ ! -z "$1" ]; do
+    case "$1" in
+	gpu)
+	    echo "GPU mode selected"
+	    args+=(-R "rusage[ngpus_excl_p=1]")
+	    ;;
+	intr)
+	    echo "Interactive mode selected"
+	    args+=(-Is)
+	    ;;
+    esac
+    shift
+done
+
+
+bsub "${args[@]}" -oo simplification_clustering/outputfiles/cluster.out python simplification_clustering/embed_cluster.py
+
